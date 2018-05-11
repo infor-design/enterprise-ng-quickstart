a968cf1a0733021bab16cb460d81bf6425473483
Christopher Lepore <chris.lepore@infor.com>
2018-05-11 16:51:37 -0400

    Return some html to pass tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,6 +22,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
+    debugger;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
