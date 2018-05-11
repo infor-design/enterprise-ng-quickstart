fd66789cdeff2b6f23d855b43547be17600655f0
Christopher Lepore <chris.lepore@infor.com>
2018-05-11 16:59:19 -0400

    Remove debugger

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,7 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    debugger;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
