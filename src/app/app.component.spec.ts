496d7f766e1cee7603291040c0b84e22f30db301
Theo Harper <theo.harper@infor.com>
2018-12-14 17:26:10 +0000

    Package fixes and test fixes.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,20 +12,23 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'ids-enterprise-ng-quickstart'`, async(() => {
+  });
+
+  it(`should have as title 'ids-enterprise-ng-quickstart'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('ids-enterprise-ng-quickstart');
-  }));
-  it('should render title in a h1 tag', async(() => {
+  });
+
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to ids-enterprise-ng-quickstart!');
-  }));
+  });
 });
