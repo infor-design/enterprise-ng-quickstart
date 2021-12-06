b1dc572b9ef4353505cc1c350d98dce3b7520e85
Tim McConechy <tim.mcconechy@infor.com>
2021-12-06 09:52:16 -0500

    Update to ng 13

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -31,17 +31,7 @@ import 'core-js/es/array';
 import 'core-js/es/regexp';
 import 'core-js/es/map';
 import 'core-js/es/weak-map';
-import 'core-js/es/set';
-
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/**
- * Web Animations `@angular/platform-browser/animations`
- * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
- * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'core-js/es/set';  // Run `npm install --save classlist.js`.
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
