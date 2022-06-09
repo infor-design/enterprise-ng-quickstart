15b6fc07d0241529291ed225f8ad48dc2f78928d
Tim McConechy <tim.mcconechy@infor.com>
2022-06-09 11:05:30 -0400

    Add the newest version

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,21 +18,6 @@
  * BROWSER POLYFILLS
  */
 
-import 'core-js/es/symbol';
-import 'core-js/es/object';
-import 'core-js/es/function';
-import 'core-js/es/parse-int';
-import 'core-js/es/parse-float';
-import 'core-js/es/number';
-import 'core-js/es/math';
-import 'core-js/es/string';
-import 'core-js/es/date';
-import 'core-js/es/array';
-import 'core-js/es/regexp';
-import 'core-js/es/map';
-import 'core-js/es/weak-map';
-import 'core-js/es/set';  // Run `npm install --save classlist.js`.
-
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
